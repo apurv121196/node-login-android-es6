f177343e8d1fc7e402c479b7805a47329367c651
apurv121196 <apurv121196@gmail.com>
2017-03-24 16:37:18 +0530

    manjeet bhiya

--- routes.js
+++ routes.js
@@ -10,6 +10,10 @@ const password = require('./functions/password');
 const config = require('./config/config.json');
 const img_upload = require('./functions/image_upload');
 
+
+const multer = require('multer');
+const upload = multer({ dest: '/tmp/'});
+
 module.exports = router => {
 	router.get('/',(req,res) => res.end('Welcome to AKSAK !'));
 	router.post('/authenticate',(req,res) => {
