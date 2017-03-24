3a5ee9d1ec937a7b996ac43a9cbd6fc294992f10
apurv121196 <apurv121196@gmail.com>
2017-03-24 22:07:00 +0530

    are re

--- functions/fun.js
+++ functions/fun.js
@@ -2,12 +2,14 @@ var fs = require('fs');
 
 exports.func = () => {
 	console.log('FUN ###');
-	if (process.argv.length <= 2) {
-    console.log("Usage: " + __filename + " path/to/directory");
-    // process.exit(-1);
-}
+// 	if (process.argv.length <= 2) {
+//     console.log("Usage: " + __filename + " path/to/directory");
+//     process.exit(-1);
+// }
  
-var path = process.argv[2];
+// var path = process.argv[2];
+
+var path = '/app/functions/';
  
 fs.readdir(path, function(err, items) {
     console.log(items);
