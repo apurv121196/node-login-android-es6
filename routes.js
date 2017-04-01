ab55233f893d18bbd089e7d7c22e59b66e831244
apurv121196 <apurv121196@gmail.com>
2017-04-01 11:51:07 +0530

    aksak

--- routes.js
+++ routes.js
@@ -129,7 +129,7 @@ module.exports = router => {
 		// 	console.log("wrong Token !")
 		// }
 		// res.writeHead(200, {'Content-Type': 'image/jpg' });
-		 res.sendfile('/app/functions/'+req.params.file+'.json');
+		 res.sendfile('/app/functions/'+req.params.file+'.jpg');
 		 // res.sendfile(req.params.file+'.json');
 		// res.json();
 	});
