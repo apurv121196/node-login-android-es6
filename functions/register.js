fd07b0b9488550ca1cd811c57656f0f7546d8400
apurv121196 <apurv121196@gmail.com>
2017-04-01 15:45:35 +0530

    aksak

--- functions/register.js
+++ functions/register.js
@@ -14,7 +14,7 @@ exports.registerUser = (name, email, password) =>
  
             name: name,
             email: email,
-            img:"#.jpg",
+            img:"abcd",
             hashed_password: hash,
             created_at: new Date()
         });
